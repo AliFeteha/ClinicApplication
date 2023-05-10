36614c460446da7580f5bedbb8663be24b7ea60e
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-05-10 17:18:52 +0200

    handle some errors

--- app/src/main/java/com/example/android/clinicapp/MainActivity.kt
+++ app/src/main/java/com/example/android/clinicapp/MainActivity.kt
@@ -2,12 +2,10 @@ package com.example.android.clinicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_welcome)
     }
 }
