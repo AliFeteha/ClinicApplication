bd1e7fd2df53febf41abe843cb6e3f8ba21791ac
Hedrax <88040920+Hedrax@users.noreply.github.com>
2023-05-11 00:13:27 +0300

    updating records class

---
+++ app/src/main/java/com/example/android/clinicapp/data/consts/Records.kt
@@ -0,0 +1,11 @@
+package com.example.android.clinicapp.data.consts
+
+data class Records (
+    val id :Int,
+    val patient: String,
+    val patientId: Int,
+    val doctor: String,
+    val doctorId: Int,
+    val date: String,
+    val done: Boolean
+    )
