756faad0caef119378b228e5afca8aaf6569b91d
Hedrax <88040920+Hedrax@users.noreply.github.com>
2023-05-10 22:30:55 +0300

    Resolving Main activity bug

--- app/src/main/java/com/example/android/clinicapp/App.kt
+++ app/src/main/java/com/example/android/clinicapp/App.kt
@@ -1,9 +1,9 @@
 package com.example.android.clinicapp
 
 import android.app.Application
+import android.text.format.DateFormat
 import com.example.android.clinicapp.auth.LoginViewModel
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
@@ -11,9 +11,6 @@ class App:Application() {
     override fun onCreate() {
         super.onCreate()
 
-        /**
-         * use Koin Library as a service locator
-         */
         val myModule = module {
             //Declare a ViewModel - be later inject into Fragment with dedicated injector using by viewModel()
             single {LoginViewModel(get())}
