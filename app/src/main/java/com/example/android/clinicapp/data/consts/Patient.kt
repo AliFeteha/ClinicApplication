2e9793e89c10e6521459a45f616303087f9668e6
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-05-11 14:36:49 +0200

    update id to string

--- app/src/main/java/com/example/android/clinicapp/data/consts/Patient.kt
+++ app/src/main/java/com/example/android/clinicapp/data/consts/Patient.kt
@@ -1,7 +1,7 @@
 package com.example.android.clinicapp.data.consts
 
 data class Patient(
-    var id :Int?=null,
+    var id :String?=null,
     var name :String?=null,
     var gender :String?=null,
     var email :String?=null,
