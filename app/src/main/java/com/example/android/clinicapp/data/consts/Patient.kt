152be0c4fae99ffff73d980d20bed8c7d10a7e23
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-05-12 18:32:44 +0200

    make it sorted as alphabit

--- app/src/main/java/com/example/android/clinicapp/data/consts/Patient.kt
+++ app/src/main/java/com/example/android/clinicapp/data/consts/Patient.kt
@@ -1,17 +1,19 @@
 package com.example.android.clinicapp.data.consts
 
 data class Patient(
-    var id :String?=null,
-    var name :String?=null,
-    var gender :String?=null,
-    var email :String?=null,
-    var birthDate :String?=null,
-    var imageUrl :String?=null,
     var address :String?=null,
-    var city :String?=null,
-    var mobilePhone :String?=null,
+    var birthDate :String?=null,
     var bloodType :String?=null,
-    val medicalIssues: MutableList<String> = mutableListOf(),
+    var city :String?=null,
+    var email :String?=null,
     val emergencyContact: EmergencyContact? = null,
-    val insurance: MedicalInsurance? = null
+    var gender :String?=null,
+    var id :String?=null,
+    var imageUrl :String?=null,
+    val insurance: MedicalInsurance? = null,
+    val medicalIssues: List<String> = mutableListOf(),
+    var mobilePhone :String?=null,
+    var name :String?=null,
+
+
 )
