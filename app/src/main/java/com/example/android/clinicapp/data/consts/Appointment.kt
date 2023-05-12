96c69e8263bcc79675790e4357908a9df37b2dcc
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-05-12 20:52:46 +0200

    create appointment data class

---
+++ app/src/main/java/com/example/android/clinicapp/data/consts/Appointment.kt
@@ -0,0 +1,13 @@
+package com.example.android.clinicapp.data.consts
+
+import javax.sql.StatementEvent
+
+data class Appointment(
+    var date : String,
+    var dId:String,
+    var dName:String,
+    var id:String,
+    var pId:String,
+    var pName: String,
+    var title :String
+)
