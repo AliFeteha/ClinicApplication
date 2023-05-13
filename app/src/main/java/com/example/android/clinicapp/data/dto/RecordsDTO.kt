05c87e20f0a6b2d9666760e5db5c83242536925d
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-05-13 04:09:08 +0200

    change int to strings for ids

--- app/src/main/java/com/example/android/clinicapp/data/dto/RecordsDTO.kt
+++ app/src/main/java/com/example/android/clinicapp/data/dto/RecordsDTO.kt
@@ -10,10 +10,10 @@ import java.util.*
 data class RecordsDTO(
     @ColumnInfo(name = "done") var title: Boolean?,
     @ColumnInfo(name = "patient") var pName: String?,
-    @ColumnInfo(name = "patient_id") var pId: Int?,
+    @ColumnInfo(name = "patient_id") var pId: String?,
     @ColumnInfo(name = "doctor") var dName: String?,
     @ColumnInfo(name = "date") var date: String?,
-    @ColumnInfo(name = "doctor_id") var dId: Int?,
-    @PrimaryKey @ColumnInfo(name = "id") val id:Int?
+    @ColumnInfo(name = "doctor_id") var dId: String?,
+    @PrimaryKey @ColumnInfo(name = "id") val id:String?
 )
 
