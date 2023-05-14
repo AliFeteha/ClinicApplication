4f7924b8a7446a83186df584bbb2aef15ac4afd4
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-05-14 22:29:34 +0200

    add data binding

--- app/src/main/java/com/example/android/clinicapp/dashboard/fragments/PatientDashboard.kt
+++ app/src/main/java/com/example/android/clinicapp/dashboard/fragments/PatientDashboard.kt
@@ -4,24 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.android.clinicapp.R
 import com.example.android.clinicapp.base.BaseFragment
 import com.example.android.clinicapp.base.BaseViewModel
+import com.example.android.clinicapp.databinding.FragmentLogInBinding
 import org.koin.android.ext.android.inject
 
 class PatientDashboard : BaseFragment() {
     override val _viewModel: BaseViewModel by inject()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    lateinit var binding: FragmentLogInBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patient_dashboard, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_patient_dashboard,container,false)
+        return binding.root
 
     }
 
