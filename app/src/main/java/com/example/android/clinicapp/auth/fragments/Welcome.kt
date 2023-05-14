163b3449dc71d2329d45fdf6c027ef8277d38b9d
AliFeteha <116113529+AliFeteha@users.noreply.github.com>
2023-05-14 13:25:09 +0200

    .

--- app/src/main/java/com/example/android/clinicapp/auth/fragments/Welcome.kt
+++ app/src/main/java/com/example/android/clinicapp/auth/fragments/Welcome.kt
@@ -10,7 +10,6 @@ import com.example.android.clinicapp.R
 import com.example.android.clinicapp.base.BaseFragment
 import com.example.android.clinicapp.data.consts.Days
 import com.example.android.clinicapp.data.consts.Doctor
-import com.example.android.clinicapp.data.Remote
 import com.example.android.clinicapp.databinding.FragmentWelcomeBinding
 import org.koin.android.ext.android.inject
 
@@ -28,13 +27,6 @@ class Welcome : BaseFragment() {
             inflater, R.layout.fragment_welcome, container, false
         )
         binding.viewModel = _viewModel
-        val r = Remote()
-        /*lifecycleScope.launch(Dispatchers.Main) {
-            var a:MutableList<Appointment> =r.getAllAppointments();
-            Log.i("Aaa",a.toString())
-        }*/
-        r.signUpDoctor(Doctor("cairo","aa","ssss","1235","aaaaa","ali","alii", "01212122121",listOf(Days.Friday)),"aaaaaaaaaa")
-        Log.i("hey","from welcome")
         return binding.root
     }
 }
